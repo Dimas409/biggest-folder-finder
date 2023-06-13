429451a9aa23f3297b7888967646ae807e115262
Dimas409 <82093956+Dimas409@users.noreply.github.com>
2023-06-13 15:35:06 +0300

    dev

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,22 @@
 import java.io.File;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
-        String folderPath = "/home/dmitry/Desktop/folderNew";
+        String folderPath = "D:\\SteamLibrary";
         File file = new File(folderPath);
-        System.out.println(getFolderSize(file));
+        long start = System.currentTimeMillis();
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + "ms");
+//        System.out.println(getFolderSize(file));
+
+
 
 
     }
